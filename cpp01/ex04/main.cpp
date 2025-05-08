9ec79109af101f9d6e1b320a60acb2c218b1f136
Felipe Caldas Marques Nasser <103406806+afsser@users.noreply.github.com>
2025-05-07 21:16:07 -0300

    cpp01/ex04 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,29 +5,48 @@
 
 static int	open_file(std::string infileName, std::string outfileName, std::ifstream *infile, std::ofstream *outfile)
 {
-	(*infile).open(infileName, std::fstream::in);
-	(*outfile).open(outfileName, std::fstream::out);
-	if (!infile || !outfile)
+	(*infile).open(infileName.c_str(), std::fstream::in);
+	(*outfile).open(outfileName.c_str(), std::fstream::out);
+	if (!infile->is_open() || !outfile->is_open())
 	{
 		std::cerr << "Failed to open files" << std::endl;
 		(*infile).close();
-		(*infile).close();
+		(*outfile).close();
 		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
 
-void	copy_and_replace(std::ifstream *infile, std::ofstream *outfile, std::string str_sub, std::string str_add)
+static int	copy_and_replace(std::ifstream *infile, std::ofstream *outfile, std::string str_sub, std::string str_add)
 {
-	std::string::size_type	i;
-	std::string::size_type	i;
-	std::string				line;
+	size_t			pos;
+	size_t			found;
+	std::string		result;
+	std::string		line;
+	if (str_sub.empty())
+	{
+		std::cerr << "Empty string to replace" << std::endl;
+		return (EXIT_FAILURE);
+	}
 	while (getline(*infile, line))
 	{
-		
-	}
-	
+		result = "";
+		pos = 0;
 
+		while (true)
+		{
+			found = line.find(str_sub, pos);
+			result.append(line, pos, found - pos);
+			if (found == std::string::npos)
+				break ;
+			result.append(str_add);
+			pos = found + str_sub.length();
+		}
+		(*outfile) << result;
+		if (!infile->eof())
+			(*outfile) << std::endl;
+	}
+	return (EXIT_SUCCESS);
 }
 
 int	main(int argc, char **argv)
@@ -40,14 +59,14 @@ int	main(int argc, char **argv)
 	std::ofstream	outfile;
 
 	if (argc != 4)
-		return (std::cout << "Wrong number of arguments\n./ft_sed <file> <s1> <s2>" << std::endl, EXIT_FAILURE);
+		return (std::cerr << "Wrong number of arguments\n./ft_sed <file> <s1> <s2>" << std::endl, EXIT_FAILURE);
 	outfileName = infileName + ".replace";
 	if (open_file(infileName, outfileName, &infile, &outfile))
 		return (EXIT_FAILURE);
-	copy_and_replace(&infile, &outfile, str_sub, str_add);
+	if (copy_and_replace(&infile, &outfile, str_sub, str_add))
+		return (EXIT_FAILURE);
 	infile.close();
 	outfile.close();
 
-	std::size_t found = infileName.find_first_of(str_sub[0]);
 	return (EXIT_SUCCESS);	
 }

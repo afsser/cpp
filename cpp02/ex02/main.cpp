08c71244bbc785fc18835a9ff8706b24440717ba
Felipe Caldas Marques Nasser <103406806+afsser@users.noreply.github.com>
2025-05-08 19:03:37 -0300

    :pencil: draft: aditional tests + ex03 init

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,10 +6,6 @@ int main( void ) {
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	Fixed c(b);
 	std::cout << "a = " << a << std::endl;
-	for (int i = 0; i < 256; i++)
-		++a;
-	std::cout << "a = " << a << std::endl;
-	a = 0;
 	std::cout << "++a = " << ++a << std::endl;
 	std::cout << "a = " << a << std::endl;
 	std::cout << "a++ = " << a++ << std::endl;
@@ -21,5 +17,13 @@ int main( void ) {
 	std::cout << "&min a/b = " << &Fixed::min( a, b ) << std::endl;
 	std::cout << "c = " << c << std::endl;
 	std::cout << "++a = " << (c / Fixed(2)) << std::endl;
+
+	std::cout << "\nExtra test:" << std::endl;
+	a = 0;
+	std::cout << "a = " << a << std::endl;
+	for (int i = 0; i < 256; i++)
+		++a;
+	std::cout << "After calling ++a 256 times:" << std::endl;
+	std::cout << "a = " << a << std::endl;
 	return 0;
 }

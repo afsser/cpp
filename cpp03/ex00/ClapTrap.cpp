657dbafa9f9daf5e8519021a2e3f79b210c11b60
Felipe Caldas Marques Nasser <103406806+afsser@users.noreply.github.com>
2025-05-13 19:17:39 -0300

    :pencil: draft: cpp03/ex00 finished

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -5,11 +5,11 @@ ClapTrap::ClapTrap() {
 }
 
 ClapTrap::~ClapTrap() {
-	std::cout << BOLD << "ClapTrap" << this->_name << "destroyed" << RST << std::endl;
+	std::cout << BOLD << "ClapTrap " << BLU << this->_name << BOLD << " destroyed" << RST << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &copy) {
-	std::cout << BOLD << "ClapTrap " << this->_name << " copied" << RST << std::endl;
+	std::cout << BOLD << "ClapTrap " << BLU << this->_name << BOLD << " copied" << RST << std::endl;
 	*this = copy;
 }
 
@@ -32,10 +32,15 @@ unsigned int	ClapTrap::getAtkDmg(void) const {
 	return (this->_atkDmg);
 }
 
+void	ClapTrap::setAtkDmg(unsigned int atkDmg) {
+	this->_atkDmg = atkDmg;
+	std::cout << YEL << "ClapTrap " << BLU << this->_name << YEL << "'s attack damage set to " << RED << this->_atkDmg << YEL << "!" << RST << std::endl;
+}
+
 void	ClapTrap::attack(const std::string& target) {
 	if (this->_ep > 0) {
 		this->_ep--;
-		std::cout << YEL << "ClapTrap " << this->_name << " attacks " << target << ", causing " << RED << this->_atkDmg << YEL << " points of damage!" << RST << std::endl;
+		std::cout << YEL << "ClapTrap " << BLU << this->_name << YEL << " attacks " << target << " with " << RED << this->_atkDmg << YEL << " damage!" << RST << std::endl;
 	}
 	else if (this->_ep == 0) {
 		std::cout << YEL << this->_name << " don't have enough energy to attack " << target << " !" << RST << std::endl;
@@ -45,25 +50,28 @@ void	ClapTrap::attack(const std::string& target) {
 void	ClapTrap::takeDamage(unsigned int amount) {
 	if (this->_hp > 0) {
 		this->_hp -= amount;
-		std::cout << YEL << "ClapTrap " << this->_name << " takes " << RED << amount << YEL << " damage!" << RST << std::endl;
-		std::cout << YEL << "ClapTrap " << this->_name << " now have " << RED << this->_hp << YEL << " hitpoints!" << RST << std::endl;
+		std::cout << YEL << "ClapTrap " << BLU << this->_name << YEL << " takes " << RED << amount << YEL << " damage!" << RST << std::endl;
+		std::cout << YEL << "ClapTrap " << BLU << this->_name << YEL << " now have " << RED << this->_hp << YEL << " hitpoints!" << RST << std::endl;
 	}
 	else if (this->_hp == 0) {
-		std::cout << YEL << this->_name << " is already fainted!" << RST << std::endl;
+		std::cout << YEL << "ClapTrap " << BLU << this->_name << RED << " is already fainted!" << RST << std::endl;
 	}
 }
 
 void	ClapTrap::beRepaired(unsigned int amount) {
-	if (this->_ep > 0) {
+	if (this->_ep > 0 && this->_hp < 10) {
 		this->_ep--;
 		this->_hp += amount;
-		std::cout << YEL << "ClapTrap " << this->_name << " got repaired by " << GRN << amount << YEL << " points!" << RST << std::endl;
+		std::cout << YEL << "ClapTrap " << BLU << this->_name << YEL << " got repaired by " << GRN << amount << YEL << " points!" << RST << std::endl;
+	}
+	else if (this->_hp == 10) {
+		std::cout << YEL << "ClapTrap " << BLU << this->_name << GRN << " is already at full health!" << RST << std::endl;
 	}
 	else if (this->_ep == 0) {
-		std::cout << YEL << this->_name << " don't have enough energy to repair!" << RST << std::endl;
+		std::cout << YEL << "ClapTrap " << BLU << this->_name << YEL << " don't have enough energy to repair!" << RST << std::endl;
 	}
 }
 
 ClapTrap::ClapTrap(std::string name) : _name(name), _hp(10), _ep(10), _atkDmg(0) {
-	std::cout << "ClapTrap " << name << " created" << std::endl;
+	std::cout << BOLD << "ClapTrap " << BLU << name << BOLD <<" created" << std::endl;
 }
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -26,6 +26,7 @@ public:
 	ClapTrap(std::string name);
 	std::string 	getName(void) const;
 	unsigned int	getAtkDmg(void) const;
+	void			setAtkDmg(unsigned int atkDmg);
 	void			attack(const std::string& target);
 	void			takeDamage(unsigned int amount);
 	void			beRepaired(unsigned int amount);

f0883ad74d0e9e31a7a174c356734eed0d20bdfb
felipe nasser <felipecmnasser@gmail.com>
2025-07-08 15:20:52 -0300

    :boom: cpp09 init

---
+++ cpp09/ex02/colors.hpp
@@ -0,0 +1,14 @@
+#ifndef COLORS_HPP
+# define COLORS_HPP
+
+# define BOLD "\033[1;37m"
+# define RED "\033[1;31m"
+# define GRN "\033[1;32m"
+# define YEL "\033[1;33m"
+# define MAG "\033[1;35m"
+# define BLU "\033[1;36m"
+# define ORG "\033\[1m\033[38;2;255;165;0m"
+# define RST "\033[0m"
+
+
+#endif
